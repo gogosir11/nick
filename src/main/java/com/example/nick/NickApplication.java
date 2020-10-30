85de5633f08ebcaf710a09b1bcdbd87d282aa8ef
gogosir11 <1145412843@qq.com>
2020-10-30 08:22:52 +0800

    first

---
+++ src/main/java/com/example/nick/NickApplication.java
@@ -0,0 +1,13 @@
+package com.example.nick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NickApplication.class, args);
+    }
+
+}
